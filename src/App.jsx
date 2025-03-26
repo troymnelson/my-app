26b8ed4ae91978f804ca1fd2c12d5cf1957a692a
troymnelson <troymn625@gmail.com>
2025-03-26 16:03:26 -0500

    browserrouter addded

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './components/Home'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
 import About from './components/About'
+import NotFound from "./components/NotFound"
 
 function App() {
    
@@ -16,6 +17,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />  
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
     </>
